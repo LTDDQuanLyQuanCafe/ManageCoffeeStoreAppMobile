678be6ee8b1ed8afc2fea4b79c9f09a4d42cd5a0
ntNgot <nt.280420@gmail.com>
2021-07-15 15:23:39 +0700

    add search activity

---
+++ TaiThanhTuan__TranThiNgocThao_UngDungMobileQuanLyQuanCafe/app/src/main/java/com/example/taithanhtuan__tranthingocthao_ungdungmobilequanlyquancafe/ChiTietThucDonActivity.java
@@ -0,0 +1,14 @@
+package com.example.taithanhtuan__tranthingocthao_ungdungmobilequanlyquancafe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChiTietThucDonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_thuc_don);
+    }
+}

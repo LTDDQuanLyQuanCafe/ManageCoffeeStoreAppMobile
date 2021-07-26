7a9009f3b71018ccc64051b682e5dd7d1fe94165
ntNgot <nt.280420@gmail.com>
2021-07-27 06:14:13 +0700

    chinhgiohang

--- TaiThanhTuan__TranThiNgocThao_UngDungMobileQuanLyQuanCafe/app/src/main/java/com/example/taithanhtuan__tranthingocthao_ungdungmobilequanlyquancafe/common/Common.java
+++ TaiThanhTuan__TranThiNgocThao_UngDungMobileQuanLyQuanCafe/app/src/main/java/com/example/taithanhtuan__tranthingocthao_ungdungmobilequanlyquancafe/common/Common.java
@@ -6,11 +6,7 @@ import com.example.taithanhtuan__tranthingocthao_ungdungmobilequanlyquancafe.dal
 import java.util.ArrayList;
 
 public class Common {
-<<<<<<< HEAD
     public static String preUrl = "https://192.168.1.4:5566/api/";
-=======
-    public static String preUrl = "https://192.168.1.9:5566/api/";
->>>>>>> 4ef7f275a2926a1a6fa461e5890a79b1e842d87e
 
     //    public static NGUOIDUNG USER;
     public static ArrayList<GioHang> carts = new ArrayList<>();
